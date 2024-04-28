c64fafd2f3ecaa8402cb88aa6e47b53b0fe45965
SehajbirSingh <singhsehajbirs1906@gmail.com>
2024-04-28 18:56:24 +0530

    Final Call

---
+++ assets/automatic_typing.js
@@ -0,0 +1,7 @@
+ var typed = new Typed('.sehaj', {
+            strings: ["Help A Special Verto","Become A Cool Verto !"],
+            typeSpeed: 130,
+            backSpeed: 130,
+            loop:true
+});
+   
